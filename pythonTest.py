97e3f835dec94a064cf09b71a23b4401c40e4dc4
jamesmalek <75370446+jamesmalek@users.noreply.github.com>
2023-08-20 19:47:55 +0100

    Update pythonTest.py

--- pythonTest.py
+++ pythonTest.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from ansi2html import Ansi2HTMLConverter
 
 x = [1, 2, 3, 4, 5]
 y = [10, 20, 15, 30, 25]
@@ -7,4 +8,8 @@ plt.plot(x, y)
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Sample Plot')
-plt.savefig('sample_plot.png')
+
+# Convert Matplotlib output to HTML
+converter = Ansi2HTMLConverter()
+html_output = converter.convert(plt.gcf()._repr_html_())
+print(html_output)
